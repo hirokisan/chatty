d3050f14ac2f5df5a3332d2b2e0bb76ef4411927
hirokisan <dev@hirokiueno.com>
2023-03-12 17:54:10 +0900

    fix: because the file was unintentionally closed

--- main.go
+++ main.go
@@ -12,12 +12,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func main() {
-	if err := cmd.Execute(); err != nil {
-		log.Fatal(err)
-	}
-}
-
 var cmd = &cobra.Command{
 	Use:   "chatty",
 	Short: "Would you like to have a little chat with us between jobs?",
@@ -30,12 +24,23 @@ var cmd = &cobra.Command{
 		}
 		client := openai.NewClient(key)
 
-		messenger, err := getMessenger()
-		if err != nil {
-			return fmt.Errorf("get messenger: %w", err)
+		var messenger message.Messenger
+		{
+			messagesPath, acceptMessageHistory := os.LookupEnv("CHATTY_MESSAGES_PATH")
+			if acceptMessageHistory {
+				file, err := os.OpenFile(messagesPath, os.O_RDWR|os.O_CREATE, 0666)
+				if err != nil {
+					return fmt.Errorf("create or open file: %w", err)
+				}
+				defer file.Close()
+				messenger = message.NewHistoricalMessenger(file)
+			} else {
+				messenger = message.NewIndependentMessenger()
+			}
 		}
 
 		msg := strings.Join(args, " ")
+
 		reply, err := messenger.GetReply(ctx, client, msg)
 		if err != nil {
 			return fmt.Errorf("get reply: %w", err)
@@ -46,16 +51,8 @@ var cmd = &cobra.Command{
 	},
 }
 
-func getMessenger() (message.Messenger, error) {
-	messagesPath, acceptMessageHistory := os.LookupEnv("CHATTY_MESSAGES_PATH")
-	if acceptMessageHistory {
-		file, err := os.OpenFile(messagesPath, os.O_RDWR|os.O_CREATE, 0666)
-		if err != nil {
-			return nil, fmt.Errorf("create or open file: %w", err)
-		}
-		defer file.Close()
-
-		return message.NewHistoricalMessenger(file), nil
+func main() {
+	if err := cmd.Execute(); err != nil {
+		log.Fatal(err)
 	}
-	return message.NewIndependentMessenger(), nil
 }
